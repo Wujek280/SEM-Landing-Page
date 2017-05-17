28f36bb02cf60abf203acb18bd4946df83de73f3
Wujek280 <mjwojcik94@gmail.com>
2017-05-17 15:34:47 +0200

    attach onclick

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,19 @@
-﻿
+﻿'use strict()'
+
+$(function(){
+
+	
+	//Loop for attaching onclick fn to buttons
+	for(var i=0; i<document.getElementsByName('contactButton').length; i++){
+		
+		document.getElementsByName('contactButton')[i].onclick = function(){
+			popUp();
+		}
+	}
+
+	
+})
+
+function popUp(){
+	console.log('popup working');
+}
