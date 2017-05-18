ab26f0e24ea303310d0cb41e13311ec627f5a336
Wujek280 <mjwojcik94@gmail.com>
2017-05-18 20:49:01 +0200

    great animation of popup

--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,7 @@ $(function(){
 	
 })
 
-var popUpHTML = "<div class='popUpMenu'> <div class='callDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Cześć</h2> <p>Zawsze możesz do nas zadzwonić</p><a href='tel:+48664340449'> <h3>+48 664 340 449</h3> <i class='phone fa fa-phone-square fa-5x'></i></a></div><div class='replyDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Zostaw nam wiadomość</h2> <hr> <form> <button class='button-clear'>Wyczyść</button> <button class='button-send' >Wyślij</button> <label for='name'>Imię i Nazwisko</label> <br><input id='name' name='name' type='text' > <br><label for='phone'>Telefon</label> <br><input id='phone' name='phone' type='tel' placeholder='790 654 321...'> <br><label for='dropdown' >Wybierz opcję z listy</label><br><select name='dropdown' id='dropdown'> <option value='-'>-</option> <option value='lorem1'>lorem1</option> <option value='lorem2'>lorem2</option> <option value='lorem3'>lorem3</option> </select> <br><label for='message' >Wiadomość</label> <br><textarea id='message' name='message' placeholder='Napisz nam coś...'></textarea> </form> </div></div>";
+var popUpHTML = "<div class='popUpMenu show'> <div class='callDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Infolinia</h2> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><a href='tel:+48664340449'> <h3>+48 664 340 449</h3> <i class='phone fa fa-phone-square fa-5x'></i></a></div><div class='replyDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Zostaw nam wiadomość</h2> <hr> <form> <button class='button-clear'>Wyczyść</button> <button class='button-send' >Wyślij</button> <label for='name'>Imię i Nazwisko</label> <br><input id='name' name='name' type='text' > <br><label for='phone'>Telefon</label> <br><input id='phone' name='phone' type='tel' placeholder='790 654 321...'> <br><label for='dropdown' >Wybierz opcję z listy</label><br><select name='dropdown' id='dropdown'> <option value='-'>-</option> <option value='lorem1'>lorem1</option> <option value='lorem2'>lorem2</option> <option value='lorem3'>lorem3</option> </select> <br><label for='message' >Wiadomość</label> <br><textarea id='message' name='message' placeholder='Napisz nam coś...'></textarea> </form> </div></div>";
 
 var popUpMessageOnlyHTML = " <div class='popUpMenu'> <div class='replyDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Zostaw nam wiadomość</h2> <hr> <form> <button class='button-clear' >Wyczyść</button> <button class='button-send' >Wyślij</button> <label for='name'>Imię</label> <br><input id='name' name='name' type='text' > <br><label for='surname'>Nazwisko</label> <br><input id='surname' name='surname' type='text' > <br><label for='phone'>Telefon</label> <br><input id='phone' name='phone' type='tel' placeholder='790 654 321...'><br><label for='message' >Wiadomość</label> <br><textarea id='message' name='message' placeholder='Napisz nam coś...'></textarea> </form> </div></div>";
 
@@ -29,8 +29,6 @@ function addEventListeners(){
 			popUp(true);
 		}
 	}
-
-
 }
 
 function popUp(showOnlyMessagePopUp = false){
@@ -55,17 +53,20 @@ function popUp(showOnlyMessagePopUp = false){
 	document.getElementsByClassName('button-send')[0].onclick = function(event){
 			event.preventDefault();
 			formSend();
-	};
-	
+	};	
 
 	console.log('popup working');
 }
 
 
 function closePopUp(){
-	console.log('popup closed');
-	// document.body.className = '';
-	document.getElementsByClassName('backgroundDiv')[0].outerHTML = '';
+	console.log('popup closed');	
+
+	document.getElementsByClassName('popUpMenu')[0].className = 'popUpMenu hide';
+
+	setTimeout(function() {
+		document.getElementsByClassName('backgroundDiv')[0].outerHTML = '';
+    }, 600);
 }
 
 
